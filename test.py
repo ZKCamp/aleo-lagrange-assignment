97d6114397e417a1312d57d839424d834095dfb4
Shubham Kanodia <shubhamkanodia123@gmail.com>
2023-10-14 23:44:11 +0530

    Correcting TCs

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 
 import hashlib
 
-correct_solution_hash = "98e5f20e7d1af4396a5861f1941a145a"
+correct_solution_hash = "22ad632aa128cf40dc24430d21f7e4f4"
 solution_hash = hashlib.md5(open("solution.csv", "r").read().encode()).hexdigest()
 
 assert(solution_hash == correct_solution_hash)
