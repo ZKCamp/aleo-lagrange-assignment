421fc620f2ea7d17497d6d4d4b79c90ddf29d68d
Eason Chai <echai2905@gmail.com>
2023-10-18 18:57:09 +0800

    feat: Add cleaned up final solution

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
 from csv_writer import CSVWriter
+from typing import List, Tuple
 
 
-def calculate_lagrange_polynomial(points):
+def polynomial_multiplication(
+    poly1: List[int], poly2: List[int], mod: int = 17
+) -> List[int]:
+    """Multiply two polynomials in modulo arithmetic."""
+    result = [0] * (len(poly1) + len(poly2) - 1)
+    for i in range(len(poly1)):
+        for j in range(len(poly2)):
+            result[i + j] = (result[i + j] + poly1[i] * poly2[j]) % mod
+    return result
+
+
+def compute_lagrange_basis(points: List[Tuple[int, int]], j: int) -> List[int]:
+    """
+    Compute the j-th Lagrange basis polynomial.
+    It calculates the product of terms (x - x_m)/(x_j - x_m) for all m != j.
+    """
+    basis_poly = [1]
+    for m, point in enumerate(points):
+        if m == j:
+            continue
+        # (x - x_m) in polynomial form.
+        numerator = [-point[0], 1]  # [coefficient, x^power]
+        denominator = (points[j][0] - point[0]) % 17  # x_j - x_m
+        # Multiply by (x - x_m) / (x_j - x_m)
+        term = [x * pow(denominator, -1, 17) for x in numerator]
+        basis_poly = polynomial_multiplication(basis_poly, term)
+    return basis_poly
+
+
+def calculate_lagrange_polynomial(points: List[Tuple[int, int]]) -> List[int]:
     """
     Calculate lagrange polynomial.
 
@@ -16,7 +46,18 @@ def calculate_lagrange_polynomial(points):
     """
 
     # Add your code here
-    pass
+    """
+    Compute the polynomial that interpolates the given points using Lagrange's method.
+
+    We will have n-1 degrees based on n points
+    """
+    result_polynomial = [0] * len(points)
+    for point_index, (_, y) in enumerate(points):
+        for basis_poly, coeff in enumerate(compute_lagrange_basis(points, point_index)):
+            result_polynomial[basis_poly] = (
+                result_polynomial[basis_poly] + y * coeff
+            ) % 17
+    return result_polynomial
 
 
 csv_writer = CSVWriter()
